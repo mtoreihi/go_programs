65223e2f49c1316855b2620f66b5dfeef98d33ee
Mehran Toreihi <toreyhi@amnafzar.ir>
2018-01-15 16:53:29 +0330

    mongodb read data

--- webService/Handlers.go
+++ webService/Handlers.go
@@ -3,17 +3,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
-	_ "github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+	_ "gopkg.in/mgo.v2/bson"
 )
 
 func hhf_index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is a test.")
+	fmt.Fprintln(w, "This is a test for index page.")
 }
 
 func hhf_getCount(w http.ResponseWriter, r *http.Request) {
-	s := "{ count : '25'}"
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("companies")
+	cnt, err := c.Count()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := cnt
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(s); err != nil {
