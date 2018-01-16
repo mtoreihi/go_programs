7840375c29a45bdb419713aa7a9fd5176145d0be
Mehran Toreihi <toreyhi@amnafzar.ir>
2018-01-16 09:18:41 +0330

    TLS Support

--- webService/Main.go
+++ webService/Main.go
@@ -9,5 +9,7 @@ func main() {
 
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	//log.Fatal(http.ListenAndServe(":8080", router))
+	log.Println("Starting the server...")
+	log.Fatal(http.ListenAndServeTLS(":8443", "certificate.pem", "key.pem", router))
 }
