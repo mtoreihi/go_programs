141763b991c60a9765e16b95fc65eda440fa7d95
Mehran Toreihi <mtoreihi@gmail.com>
2022-11-16 09:26:22 +0330

    list_vms modified

--- list_vms/main.go
+++ list_vms/main.go
@@ -1,24 +1,3 @@
-/*
-Copyright (c) 2015 VMware, Inc. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-/*
-This example program shows how the `finder` and `property` packages can
-be used to navigate a vSphere inventory structure using govmomi.
-*/
-
 package main
 
 import (
@@ -63,14 +42,14 @@ func GetEnvBool(v string, def bool) bool {
 }
 
 const (
-	envURL      = "http://10.10.10.181"
+	envURL      = "http://192.168.10.20"
 	envUserName = ""
 	envPassword = ""
 	envInsecure = "true"
 )
 
 var urlDescription = fmt.Sprintf("ESX or vCenter URL [%s]", envURL)
-var urlFlag = flag.String("url", GetEnvString(envURL, "https://user:password@10.10.10.181/sdk"), urlDescription)
+var urlFlag = flag.String("url", GetEnvString(envURL, "https://user:password@192.168.10.20/sdk"), urlDescription)
 
 var insecureDescription = fmt.Sprintf("Don't verify the server's certificate chain [%s]", envInsecure)
 var insecureFlag = flag.Bool("insecure", GetEnvBool(envInsecure, true), insecureDescription)
